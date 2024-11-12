4926a6352b9b6352af5f91f0d794ad97211c35f2
JotaM226 <jmendozac22@outlook.com>
2024-11-11 21:38:25 -0500

    mi primer commit

---
+++ codigo.js
@@ -0,0 +1,74 @@
+/*Tipos de dato principales 
+-String(cadena de texto)
+-Number
+-Boolean */
+
+/*Tipos de dato Especiales 
+-Undefined
+-Null
+-NaN */
+
+/*
+Para Crear Variables/Constantes:
+-Let
+-Var
+-Const
+*/
+
+/*miString = "Cadena de texto";
+miNumero = 19;
+miBoleano = false;*/
+
+// const saludar = function(nombre){
+
+//     alert(`Hola ${nombre}!!, cómo has estado?!`);
+
+//     return "La función se ejecutó correctamente"
+
+// }
+
+// class Animal{
+//     constructor(especie, edad, color){
+//         this.especie = especie;
+//         this.edad = edad;
+//         this.color = color;
+//         this.info = `Soy un ${this.especie}, tengo ${this.edad} años y soy de color ${this.color} <br>`;
+//     }
+
+//     mostrarInfo(){
+//         document.write(this.info)
+//     }
+// }
+
+// class Perro extends Animal{
+//     constructor(especie, edad, color, raza){
+//         super(especie,edad,color)
+//         this.raza = raza
+//     }
+
+//     ladrar(){
+//         document.write("WAW!! <br>")
+//     }
+// }
+
+
+// let perro = new Perro("perro", "5","Blanco", "Labrador" )
+// let gato = new Animal("gato", "3","Gris" )
+// let loro = new Animal("loro", "1","verde" )
+
+// perro.ladrar()
+// perro.mostrarInfo();
+// gato.mostrarInfo();
+// loro.mostrarInfo();
+
+
+let numero = Math.random()*100
+
+numero = Math.round(numero);
+
+document.write(numero)
+
+
+
+
+
